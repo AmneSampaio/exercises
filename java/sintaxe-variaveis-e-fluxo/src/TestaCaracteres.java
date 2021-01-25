a6fb7725a32f6671d307f4ac0e9dc3d954620351
Amne Sampaio <39910460+AmneSampaio@users.noreply.github.com>
2021-01-25 16:13:59 -0300

    Adicionando exercícios de Java

---
+++ java/sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,25 @@
+
+public class TestaCaracteres {
+	
+	public static void main(String[] args) {
+		
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 35;
+		System.out.println(valor);
+		
+		String palavra = "alura cursos online " + 2020;
+		System.out.println(palavra);
+	
+	    char letra2 = 'a';
+	    System.out.println(letra);
+	
+	    char valorp = 65;           // Compila!
+	    System.out.println(valor);
+	
+	    valorp = (char) (valorp + 1);         // Não compila!
+	    System.out.println(valorp);
+			
+		}
+}
