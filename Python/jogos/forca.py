f3f4f704db2ef1799ab48a6c73ab5d89a50f038e
Amne Sampaio <39910460+AmneSampaio@users.noreply.github.com>
2021-02-09 20:09:03 -0300

    Removendo diretorios antigos e adicionando parametros Java ao .gitignore

--- Python/jogos/forca.py
+++
@@ -1,7 +0,0 @@
-
-print("*********************************")
-print("***Bem vindo ao jogo da forca!***")
-print("*********************************")
-
-
-print("Foi muito bom jogar com você! Fim do jogo")
